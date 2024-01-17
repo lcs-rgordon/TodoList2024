0d9ff3d74d58631e6972e42ffe1570e476c381f8
Russell Gordon <rgordon@lcs.on.ca>
2024-01-17 17:54:09 -0500

    Initial Commit

---
+++ TodoList2024/TodoList2024App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList2024App.swift
+//  TodoList2024
+//
+//  Created by Russell Gordon on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct TodoList2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
